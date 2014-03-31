8d1ade76bcfff0ef7d6d01bd1b5486b8369d3473
tdq <nikolay.a.gorokhov@gmail.com>
2014-03-31 20:04:39 +0700

    Add launcher icon

--- CompositeLauncher/src/compositelauncher/actions/launching/CompositeLauncherDelegate.java
+++ CompositeLauncher/src/compositelauncher/actions/launching/CompositeLauncherDelegate.java
@@ -10,6 +10,7 @@ import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchManager;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+import org.eclipse.debug.ui.DebugUITools;
 
 import compositelauncher.actions.ui.LaunchConfigDialog;
 import compositelauncher.actions.ui.LaunchConfigDialog.LaunchConfig;
@@ -30,7 +31,8 @@ public class CompositeLauncherDelegate implements ILaunchConfigurationDelegate {
 					" delay "+config.getDelay());
 			
 			ILaunchConfiguration conf = manager.getLaunchConfiguration(config.getMemento());
-			conf.launch(config.getMode(), monitor);
+			//conf.launch(config.getMode(), monitor);
+			DebugUITools.buildAndLaunch(conf, config.getMode(), monitor);
 			
 			try {
 				Thread.sleep(config.getDelay()*1000);
